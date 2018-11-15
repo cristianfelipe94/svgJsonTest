ffa9862b33c2445967722417347c64567e96f9f5
Cristian Felipe <cristianpipe0494@gmail.com>
2018-11-15 12:08:05 -0600

    Load Response from Json.

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,31 @@
-/**
- * Project Description.
- * ...
- */
+// Function will run as Json is loaded.
+function dataReceived(event) {
+  const response = event.target.response.elements[0].type;
+  const responseDimensions = event.target.response.dimensions;
+  const responseElements = event.target.response.elements;
+  console.log(response);
+  console.log(responseDimensions);
+  console.log(responseElements);
+  responseElements.forEach(elementType => {
+    console.log(elementType);
+  });
+}
 
-const text = 'Hola mundo!';
-const container = document.getElementById('container');
-const paragraph = document.createElement('p');
-paragraph.innerText = text;
-container.appendChild(paragraph);
+// getElements(): This will create a Request to get the information from the JSON.
+function getElements() {
+  // Create a XMLHttpRequest.
+  const request = new XMLHttpRequest();
+  // Type of response the system is waiting.
+  request.responseType = 'json';
+  // Starts the request.
+  // Set the Path to get the JSON.
+  request.open('GET', '../json/datos-svg.json');
+  // Sent the request.
+  request.send();
+  console.log(request);
+  // Run the Function DataReceived as the Page Loads.
+  request.addEventListener('load', dataReceived);
+}
+
+// Set the Event to the Load Window to run the Function.
+window.addEventListener('load', getElements);
