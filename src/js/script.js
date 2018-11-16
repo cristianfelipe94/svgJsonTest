1024f3a9f922f306ce32a715d095ba39a71ffd8e
Cristian Felipe <cristianpipe0494@gmail.com>
2018-11-16 16:04:20 -0600

    Added to the DOM Shapes using SVG.

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,43 @@
 // Function will run as Json is loaded.
 function dataReceived(event) {
-  const response = event.target.response.elements[0].type;
   const responseDimensions = event.target.response.dimensions;
   const responseElements = event.target.response.elements;
-  console.log(response);
-  console.log(responseDimensions);
-  console.log(responseElements);
+  const domContainer = document.getElementById('container');
+  const svgContainer = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+  domContainer.appendChild(svgContainer);
+  const containerWidth = responseDimensions.width;
+  const containerHeight = responseDimensions.height;
+  svgContainer.setAttribute('width', `${containerWidth}`);
+  svgContainer.setAttribute('height', `${containerHeight}`);
+  /* eslint-disable-next-line */
   responseElements.forEach(elementType => {
-    console.log(elementType);
+    if (elementType.type === 'circle') {
+      const elementFigure = elementType.type;
+      const svgShape = document.createElementNS('http://www.w3.org/2000/svg', `${elementFigure}`);
+      svgContainer.appendChild(svgShape);
+      const cxElement = elementType.attributes.cx;
+      const cyElement = elementType.attributes.cy;
+      const rElement = elementType.attributes.r;
+      const fillElement = elementType.attributes.fill;
+      svgShape.setAttribute('cx', `${cxElement}`);
+      svgShape.setAttribute('cy', `${cyElement}`);
+      svgShape.setAttribute('r', `${rElement}`);
+      svgShape.setAttribute('fill', `${fillElement}`);
+    } else {
+      const elementFigure = elementType.type;
+      const svgShape = document.createElementNS('http://www.w3.org/2000/svg', `${elementFigure}`);
+      svgContainer.appendChild(svgShape);
+      const xElement = elementType.attributes.x;
+      const yElement = elementType.attributes.y;
+      const widthElement = elementType.attributes.width;
+      const heightElement = elementType.attributes.height;
+      const fillElement = elementType.attributes.fill;
+      svgShape.setAttribute('x', `${xElement}`);
+      svgShape.setAttribute('y', `${yElement}`);
+      svgShape.setAttribute('width', `${widthElement}`);
+      svgShape.setAttribute('height', `${heightElement}`);
+      svgShape.setAttribute('fill', `${fillElement}`);
+    }
   });
 }
 
@@ -22,7 +52,6 @@ function getElements() {
   request.open('GET', '../json/datos-svg.json');
   // Sent the request.
   request.send();
-  console.log(request);
   // Run the Function DataReceived as the Page Loads.
   request.addEventListener('load', dataReceived);
 }
